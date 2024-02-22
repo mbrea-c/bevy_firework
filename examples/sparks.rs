c2645a7e865341fba865c290713deca36755cb86
Manuel Brea <m.brea.carreras@gmail.com>
2024-02-22 15:38:10 +0000

    Fixed running without default features, added stress tests

--- examples/sparks.rs
+++ examples/sparks.rs
@@ -8,12 +8,16 @@ use bevy_utilitarian::prelude::*;
 use std::f32::consts::PI;
 
 fn main() {
-    App::new()
-        .add_plugins(DefaultPlugins)
+    let mut app = App::new();
+    app.add_plugins(DefaultPlugins)
         .add_plugins(ParticleSystemPlugin)
         .add_systems(Startup, setup)
-        .add_systems(Update, adjust_time_scale)
-        .run();
+        .add_systems(Update, adjust_time_scale);
+
+    #[cfg(feature = "physics_xpbd")]
+    app.add_plugins(bevy_xpbd_3d::prelude::PhysicsPlugins::default());
+
+    app.run();
 }
 
 /// set up a simple 3D scene
